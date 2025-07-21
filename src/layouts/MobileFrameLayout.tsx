892993652b4a1135fc77a8c1f7960b3755181c81
김수지 <sj.kim10@okestro.com>
2025-07-21 16:44:17 +0900

    feat: 컴포넌트 폴더링

--- src/layouts/MobileFrameLayout.tsx
+++ src/layouts/MobileFrameLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from '../components/Header';
-import DockBar from '../components/DockBar';
+import Header from '../components/layout/Header';
+import DockBar from '../components/layout/DockBar';
 
 const MobileFrameLayout = () => {
   return (
